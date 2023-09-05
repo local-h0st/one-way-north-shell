4b43ad2d59aaeb28e6ae60e1b556c6e2166835b6
redh3tALWAYS <redh3tALWAYS@example.com>
2023-09-05 03:28:46 -0400

    parse input done, but redirect is not supported.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <unistd.h>     // getcwd
 #include <sys/types.h>  // uid_t
 #include <pwd.h>        // getpwuid
+#include <string.h>     // strcpy
+#include <stdlib.h>     // malloc
+#include <ctype.h>      // isspace
 
 #define STATE_CWD_LEN 128
 #define STATE_INPUT_LEN 128
@@ -14,12 +17,37 @@ struct COMMAND_FRAG {
     // TODO redirect
 };
 
+int initCmdFragNull(struct COMMAND_FRAG *frag){
+    frag->binname = NULL;
+    frag->arg = NULL;
+    frag->pipe_next = NULL;
+    frag->arg_next = NULL;
+    return 4;
+}
+
+int deleteFragAll(struct COMMAND_FRAG *frag){
+    // will left the root node
+    if(frag->binname!=NULL)
+        free(frag->binname);
+    if(frag->arg!=NULL)
+        free(frag->arg);
+    if(frag->pipe_next!=NULL){
+        deleteFragAll(frag->pipe_next);
+        free(frag->pipe_next);
+    }
+    if(frag->arg_next!=NULL){
+        deleteFragAll(frag->arg_next);
+        free(frag->arg_next);
+    }
+    return initCmdFragNull(frag);
+}
+
 struct STATE_STRUCT {
     char cwd[STATE_CWD_LEN];
     char input[STATE_INPUT_LEN];
     struct passwd *current_passwd;
 
-    struct COMMAND_FRAG *command;
+    struct COMMAND_FRAG *command;   // remember to delete after every loop.
 } State;
 
 
@@ -38,38 +66,85 @@ void prompt(){
 
 void printState(){
     // for debug
-    printf("cwd: %s\n", State.cwd);
-    printf("input: %s\n", State.input);
+    struct COMMAND_FRAG * bin = State.command;
+    while(bin!=NULL){
+        printf("binname: %s\n",bin->binname);
+        struct COMMAND_FRAG * arg = bin->arg_next;
+        while(arg!=NULL){
+            printf("arg: %s\n",arg->arg);
+            arg = arg->arg_next;
+        }
+        bin = bin->pipe_next;
+    }
 }
 
 void printWelcome(){
-    printf("  ____             _      __            _  __         __  __    \n");
-    printf(" / __ \\___  ___   | | /| / /__ ___ __  / |/ /__  ____/ /_/ /    \n");
-    printf("/ /_/ / _ \\/ -_)  | |/ |/ / _ `/ // / /    / _ \\/ __/ __/ _ \\   \n");
-    printf("\\____/_//_/\\__/   |__/|__/\\_,_/\\_, / /_/|_/\\___/_/  \\__/_//_/   \n");
-    printf("                              /___/                             \n");
+    printf("  ____             _      __            _  __         __  __        \n");
+    printf(" / __ \\___  ___   | | /| / /__ ___ __  / |/ /__  ____/ /_/ /       \n");
+    printf("/ /_/ / _ \\/ -_)  | |/ |/ / _ `/ // / /    / _ \\/ __/ __/ _ \\    \n");
+    printf("\\____/_//_/\\__/   |__/|__/\\_,_/\\_, / /_/|_/\\___/_/  \\__/_//_/ \n");
+    printf("                              /___/                                 \n");
     printf("\n");
+    printf("                             redh3tALWAYS                           \n");
+    printf("\n");
+
+    // do something;
+    State.command = (struct COMMAND_FRAG *)malloc(sizeof(struct COMMAND_FRAG));
+    initCmdFragNull(State.command);
 }
 
 int parseCommand(){
+    deleteFragAll(State.command);
     char temp[STATE_INPUT_LEN];
     int temp_index = 0;
-    int is_binname = 1;
-    for(int i = 0; i < STATE_INPUT_LEN || State.input[i] == '\0'; i++){
+    int is_binname = 2; // 2 the first, 1 after |, 0 the arg.
+    struct COMMAND_FRAG *bin_head = NULL;
+    struct COMMAND_FRAG *last_arg = NULL;
+    for(int i = 0; i < STATE_INPUT_LEN && State.input[i] != '\0'; i++){ // last is \n and then is \0
         if (!isspace(State.input[i]) && State.input[i] != '|'){
             temp[temp_index] = State.input[i];
             temp_index++;
         }
-        else if (State.input[i] == '|'){
-
-        }
-        else if (temp_index == 0){
-            // might | and blank at same time
-            continue;
-        }
         else{
-
+            // printf("temp_index: %d, is_binname: %d\n",temp_index, is_binname);
+            // encountered blank char or '|'
+            if (temp_index == 0){
+                if (State.input[i] == '|')
+                    is_binname = 1;
+                continue;   // ignore blank
+            }
+            temp[temp_index] = '\0';
+            switch (is_binname) {
+            case 2:
+                State.command->binname = (char *)malloc((temp_index+1)*sizeof(char));
+                strcpy(State.command->binname,temp);    // need \0 ?
+                is_binname = 0;
+                bin_head = State.command;
+                last_arg = bin_head;
+                break;
+            case 1:
+                bin_head->pipe_next = (struct COMMAND_FRAG *)malloc(sizeof(struct COMMAND_FRAG));
+                bin_head = bin_head->pipe_next;
+                initCmdFragNull(bin_head);
+                bin_head->binname = (char *)malloc((temp_index+1)*sizeof(char));
+                strcpy(bin_head->binname, temp);    // need \0 ?
+                is_binname = 0;
+                last_arg = bin_head;
+                break;
+            default:
+                // is_binname = 0, default is common arg.
+                last_arg->arg_next = (struct COMMAND_FRAG *)malloc(sizeof(struct COMMAND_FRAG));
+                last_arg = last_arg->arg_next;
+                initCmdFragNull(last_arg);
+                last_arg->arg = (char *)malloc((temp_index+1)*sizeof(char));
+                strcpy(last_arg->arg, temp);
+                break;
+            }
+            temp_index =0;
         }
+
+        if (State.input[i] == '|')
+            is_binname = 1;
     }
 }
 
@@ -78,6 +153,7 @@ int main(void){
     while(1){
         updateState();
         prompt();
+        parseCommand();
         printState();
     }
     return 0;
